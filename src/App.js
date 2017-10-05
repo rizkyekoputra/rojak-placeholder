17a24b5cdc621f8c14294a1c9d90efc4b2a59775
Imre Nagi <imre.nagi2812@gmail.com>
2017-10-05 12:23:39 -0700

    Update description

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
             Project kawalmedia.id adalah sebuah non-profit project untuk membantu jalannya proses pemilihan umum baik itu pilkada atau pilpres dalam hal pengawasan media daring (online). Dengan mengusung nilai-nilai kerelawanan, kolaborasi, transparansi dan objektivitas kami berharap mampu menjadi pusat informasi alternatif bagi para pemilih dan membantu mengurangi gesekan yang timbul saat musim pemilu.
         </p>
         <p className="App-intro">
-            Dengan bantuan kawalmedia.id kita bisa tau media mana saja yang pro/kontra pasangan kandidat, bagaimana trend politik menghadapi pilkada dan pilpres di Indonesia dari hari ke hari berdasarkan data seluruh media daring di Indonesia, kandidat mana yang paling banyak di bahas oleh media dan lain-lain.
+            Dengan bantuan kawalmedia.id masyarakat dapat mengetahui bagaimana sentiment setiap media terhadap kandidat, bagaimana trend politik menghadapi pilkada dan pilpres di Indonesia dari hari ke hari berdasarkan data seluruh media daring di Indonesia, kandidat mana yang paling banyak di bahas oleh media dan lain-lain.
         </p>
       </div>
     );
